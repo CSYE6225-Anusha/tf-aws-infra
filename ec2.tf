572067a6cea013acb240f1d1c7c17102a8d0a1d6
Anusha Kadali <kadali.a@northeastern.edu>
2024-10-21 17:42:39 -0400

    feat: added rds

--- ec2.tf
+++ ec2.tf
@@ -52,11 +52,89 @@ resource "aws_security_group" "application_security_group" {
   }
 }
 
+// security group for rds instance
+resource "aws_security_group" "database_security_group" {
+  name        = "database_security_group"
+  description = "Security group for RDS instance"
+  vpc_id      = aws_vpc.main.id
+
+  # Ingress rule to allow SSH (Port 22)
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = var.ssh_port
+    to_port     = var.ssh_port
+    protocol    = var.protocol
+    cidr_blocks = [var.destination_cidr_zero]
+  }
+
+  # Ingress rule to allow Postgres (Port 5432)
+  ingress {
+    description = "Allow PostgreSQL traffic from application security group"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = var.protocol
+    security_groups = [aws_security_group.application_security_group.id]
+  }
+
+  # Egress rule to allow all outbound traffic
+  egress {
+    from_port   = var.outbound_port
+    to_port     = var.outbound_port
+    protocol    = var.outbound_protocol
+    cidr_blocks = [var.destination_cidr_zero]
+  }
+
+  tags = {
+    Name = "database_security_group"
+  }
+}
+
+# RDS Parameter Group for the database
+resource "aws_db_parameter_group" "private_db_parameter_group" {
+  name        = "custom-db-parameter-group"
+  family      = "postgres16"
+  description = "Custom parameter group for the RDS instance"
+  parameter {
+    name  = "log_min_duration_statement"
+    value = "1000"   # Log queries that take longer than 1 second
+  }
+
+  tags = {
+    Name = "custom-db-parameter-group"
+  }
+}
+
+resource "aws_db_subnet_group" "private_db_subnet_group" {
+  name = "private_db_subnet_group"
+  subnet_ids = [for i in aws_subnet.subnets_private : i.id]
+}
+
 data "aws_ami" "my_ami" {
   most_recent = true
   filter {
     name   = "name"
     values = ["csye6225*"]
+  }
+}
+
+// Database Instance
+resource "aws_db_instance" "db_instance" {
+  engine               = var.engine
+  engine_version       = var.engine_version
+  instance_class       = var.instance_class
+  multi_az             = var.multi_az
+  identifier           = var.identifier
+  username             = var.username
+  password             = var.password
+  allocated_storage = 20
+  db_subnet_group_name = aws_db_subnet_group.private_db_subnet_group.name
+  publicly_accessible = var.publicly_accessible
+  db_name              = var.db_name
+  vpc_security_group_ids = [aws_security_group.database_security_group.id]
+  skip_final_snapshot = true
+  parameter_group_name = aws_db_parameter_group.private_db_parameter_group.name
+  tags={
+    Name = "My Database Instance"
   }
 }
 
@@ -77,6 +155,23 @@ resource "aws_instance" "app_instance" {
     volume_type           = var.volume_type
     delete_on_termination = var.delete_on_termination
   }
+
+  user_data = <<-EOF
+    #!/bin/bash
+    cd /opt/csye6225/app/webapp
+    touch .env
+    echo "PORT=${var.port}" >> .env
+    echo "DB_NAME=${aws_db_instance.db_instance.db_name}" >> .env
+    echo "DB_USERNAME=${aws_db_instance.db_instance.username}" >> .env
+    echo "DB_PASSWORD=${aws_db_instance.db_instance.password}" >> .env
+    echo "host=${aws_db_instance.db_instance.address}" >> .env
+    echo "dialect=${var.dialect}" >> .env
+    sudo systemctl daemon-reload
+    sudo systemctl enable webapp.service
+    
+  EOF
+
+  # keep ssl shit in sequalize : TODO and aslo add start of webapp.service in user_data
 
   # Tag the instance
   tags = {
--- variables.tf
+++ variables.tf
@@ -100,3 +100,64 @@ variable "key-pair" {
   type    = string
   default = "my-key-pair"
 }
+
+# Database Name
+variable "db_name" {
+  description = "The name of the database."
+  type        = string
+}
+
+# Database Engine
+variable "engine" {
+  description = "The database engine to use (e.g., postgres)."
+  type        = string
+}
+
+# Engine Version
+variable "engine_version" {
+  description = "The version of the database engine."
+  type        = string
+}
+
+# Instance Class
+variable "instance_class" {
+  description = "The instance type to use for the database (e.g., db.t3.micro)."
+  type        = string
+}
+
+# Multi-AZ Deployment
+variable "multi_az" {
+  description = "Whether to deploy the database in multiple availability zones."
+  type        = bool
+}
+
+# Database Identifier
+variable "identifier" {
+  description = "The identifier for the database instance."
+  type        = string
+}
+
+# Database Username
+variable "username" {
+  description = "The master username for the database."
+  type        = string
+}
+
+# Database Password
+variable "password" {
+  description = "The password for the master username of the database."
+  type        = string
+  sensitive   = true
+}
+
+# Publicly Accessible
+variable "publicly_accessible" {
+  description = "Whether the database should be publicly accessible."
+  type        = bool
+}
+
+variable "dialect"{
+  description = "Application Dialect"
+  type = string
+  default = "postgres"
+}
